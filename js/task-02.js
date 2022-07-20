930a8e0e9f967a4d4f55ee54eac5e3a87f6e927a
Beata Pruszczyk <prubea@gmail.com>
2022-07-20 23:01:53 +0200

    Update task-02.js
    Zadanie 2 - dodawanie listy z elementami

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.innerText = ingredient;
+  ingredientsList.append(ingredientItem);
+});
