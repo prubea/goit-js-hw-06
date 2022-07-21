d8a5e2dfab31b30bc81529d3f1b5f82d5b6a65ae
Beata Pruszczyk <prubea@gmail.com>
2022-07-21 23:29:01 +0200

    Update task-10.js
    Zadanie 10 - dodatkowe
    -ustawianie pierwszym przyciskiem ilości (tworzenie kolorowych pudełek)
    - kasowanie tych pudełek za pomocą drugiego przycisku

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const input = document.querySelector("#controls input");
+const boxes = document.querySelector("#boxes");
+
+document.querySelector("[data-create]").addEventListener("click", createBoxes);
+document.querySelector("[data-destroy]").addEventListener("click", destroyBoxes);
+
+
+
+function createBoxes(amount) {
+  amount = input.value;
+  const box = [];
+  
+  for (let i = 0; i < amount; i++) {
+    let size = 30 + i * 10;
+    let color = getRandomHexColor();
+  
+    box.push(`<div style="height:${size}px ; width:${size}px ; background-color: ${color};"></div>`);
+  }
+  
+  const markup = box.join("");
+  return boxes.insertAdjacentHTML("afterbegin", markup);
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+};
