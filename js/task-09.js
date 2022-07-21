5ef2c7530dff601782c1f09f15c946a5bc4f3b0d
Beata Pruszczyk <prubea@gmail.com>
2022-07-21 23:05:46 +0200

    Update task-09.js
    Zadanie 9
    Zmiana na losowy kolor, koloru tła (użycie funkcji getRandomHexColor)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+document.querySelector(".change-color").addEventListener("click", () => {
+  const changeColor = getRandomHexColor();
+  document.querySelector(".color").innerText = changeColor;
+  document.body.style.backgroundColor = changeColor;
+  document.body.style.transition = "background 400ms ease-in-out";
+})
