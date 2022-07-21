45afbe9de4f7f006bf0dc5b90f351147cc297dfe
Beata Pruszczyk <prubea@gmail.com>
2022-07-21 22:36:16 +0200

    Zadanie 8
    Zadanie 8
    - Zdarzenie submit
    - Przy nieuzupełnieniu emaila lub password wyskakuje alert
    - użycie właściwości elements
    - czyszczenie wartości pól formularza metodą reset

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,5 @@
 const range = document.querySelector("#font-size-control");
-  const text = document.querySelector("#text");
+const text = document.querySelector("#text");
   range.addEventListener("mousemove", () => {
     text.style.fontSize = `${range.value}px`;
   });
